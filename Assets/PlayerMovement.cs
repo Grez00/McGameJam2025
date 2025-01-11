e4f810011db1deee647cd60c53b8a01dd3918025
kana797 <wangzirun68@gmail.com>
2025-01-11 16:19:38 -0500

    animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,12 +3,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    Animator animator;
+
+    private bool isMoving;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 movementInput = new Vector2 (moveX, moveY);
 
-        transform.Translate(new Vector3(moveX, moveY, 0) * speed * Time.deltaTime);
+        //change animation display according to direction
+        if (movementInput != Vector2.zero)
+        {
+            isMoving = true;
+            animator.SetBool("IsMoving", isMoving);
+            animator.SetFloat("DirectionX", movementInput.x);
+        }
+        else
+        {
+            isMoving = false;
+            animator.SetBool("IsMoving", isMoving);
+        }
+        transform.Translate(movementInput.normalized * speed * Time.deltaTime);   
     }
 }
