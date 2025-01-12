535cd2dff3cfeaf1cac9c8af3f61d202f15a4e26
Tr R <tristan.m.roque@gmail.com>
2025-01-11 19:10:46 -0500

    Camera Tracking

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float boardSize = 22.5f;
     Animator animator;
 
     private bool isMoving;
@@ -16,7 +17,12 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        Vector2 movementInput = new Vector2 (moveX, moveY);
+
+        if ((transform.position.x >= boardSize && moveX > 0) || (transform.position.x <= -boardSize && moveX < 0)) { moveX = 0; }
+        if ((transform.position.y >= boardSize && moveY > 0) || (transform.position.y <= -boardSize && moveY < 0)) { moveY = 0; }
+
+
+        Vector2 movementInput = new Vector2(moveX, moveY);
 
         //change animation display according to direction
         if (movementInput != Vector2.zero)
@@ -29,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
         {
             isMoving = false;
             animator.SetBool("IsMoving", isMoving);
-        }
+        }   
         transform.Translate(movementInput.normalized * speed * Time.deltaTime);   
     }
 }
---
+++ Assets/camera.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class CameraRunnerScript : MonoBehaviour
+{
+    public Transform player;
+    public float smoothSpeed = 0.2f;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = player.position + offset;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -20, 20);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -20, 20);
+        targetPosition.z = -20;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
+    }
+}
