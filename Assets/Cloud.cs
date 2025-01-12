5f3cd67764b9919a671893fa5ab042c5bbdb785a
Tr R <tristan.m.roque@gmail.com>
2025-01-12 10:52:37 -0500

    clouds 3, finis

--- Assets/Cloud.cs
+++ Assets/Cloud.cs
@@ -5,7 +5,6 @@ public class Cloud : MonoBehaviour
     SpriteRenderer spriteRenderer;
     public float speed = 2f;
     public Vector3 startPosition = new Vector3(25f, 25f, 0f);
-    public Vector3 endPosition = new Vector3(-25f, -25f, 0f);
 
     void OnEnable()
 {
@@ -16,14 +15,13 @@ public class Cloud : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.color = new Color(1f, 1f, 1f, .5f);
-        transform.position = startPosition;
     }
 
     void Update()
     {
         transform.Translate(new Vector3(-1, -1, 0) * speed * Time.deltaTime);
 
-        if (transform.position.x < endPosition.x || transform.position.y < endPosition.y)
+        if (transform.position.y < -45 || transform.position.x < -45)
         {
             transform.position = startPosition;
         }
