8ce73c0eb2bcc505e58f90d59f4220c83d83ef11
lilly-g <159401717+lilly-g@users.noreply.github.com>
2025-01-11 20:16:08 -0500

    fixed background music

--- Assets/AudioManagerScript.cs
+++ Assets/AudioManagerScript.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
-
 public class AudioManagerScript : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
-
     public AudioClip gameBackground;
-
     private void Start() {
         musicSource.clip = gameBackground;
         musicSource.Play();
