95c19e1a15ab6b6f77816c16190cf9e69ef24a24
Tr R <tristan.m.roque@gmail.com>
2025-01-12 13:24:00 -0500

    fixed oopsie

--- Assets/Code/Scripts/Main Menu/ButtonClick.cs
+++ Assets/Code/Scripts/Main Menu/ButtonClick.cs
@@ -10,13 +10,6 @@ public class ButtonClick : MonoBehaviour
     public string _newGameLevel;
     public static int song = 0;
     public AudioSource audioss;
-
-    public void Start()
-    {
-        audioss1 = audioss;
-        songA1 = songA;
-        songB1 = songB;
-    }
     public void NewGame ()
     {
         SceneManager.LoadScene(_newGameLevel);
