3e939c3b619d9e2601c14e9399388881e4b12d1c
yxL05 <yang.xiang.64@hotmail.com>
2025-01-12 11:01:59 -0500

    main game: audio settings

--- Assets/AudioManagerScript.cs
+++ Assets/AudioManagerScript.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
 public class AudioManagerScript : MonoBehaviour
 {
-    [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource SFXSource;
-    public AudioClip gameBackground;
-    private void Start() {
-        musicSource.clip = gameBackground;
-        musicSource.loop = true;
-        musicSource.Play();
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectspref";
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource[] backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
+
+    void Awake()
+    {
+        ContinueSettings();
+    }
+
+    private void ContinueSettings()
+    {
+        backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+        soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+
+        for (int i = 0; i < backgroundAudio.Length; i++)
+        {
+            backgroundAudio[i].volume = backgroundFloat;
+        }
+
+        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = soundEffectsFloat;
+        }
     }
 }
