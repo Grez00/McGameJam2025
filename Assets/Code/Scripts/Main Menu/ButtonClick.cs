cf2d97baa63cf030e5d8dcd93043145a399fb36f
Tr R <tristan.m.roque@gmail.com>
2025-01-12 13:19:15 -0500

    music1

--- Assets/Code/Scripts/Main Menu/ButtonClick.cs
+++ Assets/Code/Scripts/Main Menu/ButtonClick.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class ButtonClick : MonoBehaviour
 {
     public string _newGameLevel;
+    public static int song = 0;
+    public AudioSource audioss;
 
+    public void Start()
+    {
+        audioss1 = audioss;
+        songA1 = songA;
+        songB1 = songB;
+    }
     public void NewGame ()
     {
         SceneManager.LoadScene(_newGameLevel);
@@ -17,5 +26,15 @@ public class ButtonClick : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void ToggleMusic()
+    {
+        if (song == 0) {
+            song = 1;
+        }
+        else {
+            song = 0;
+        }
     }
 }
